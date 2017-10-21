e1e8605b8df46eca49d82c21fc509e1bbb121153
Bulava Eduard <bulavaeduard@gmail.com>
2017-10-21 23:24:36 +0300

    slider

---
+++ src/Image.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Carousel from 'nuka-carousel';
+
+
+class Image extends Component {
+    constructor(props) {
+        super(props);
+        this.request();
+        this.state = {
+            teams: [],
+        }
+    }
+
+    request(){
+        const FETCH_URL = `http://api.football-data.org/v1/competitions/445/teams`;
+
+        fetch('https://cors-anywhere.herokuapp.com/'+ FETCH_URL, {
+            method: 'GET',
+
+        })
+            .then(response => response.json())
+            .then(json => {
+                const teams = json.teams;
+                this.setState({ teams });
+            });
+    }
+
+
+    render() {
+        return (
+            <Carousel>
+                { this.state.teams.map((team, key) => {
+                    const img = team.crestUrl;
+                    const name = team.name;
+                    return (
+                            <img className="image" key={key} src="http://upload.wikimedia.org/wikipedia/en/6/63/Leicester02.png"/>
+                    )
+                })}
+            </Carousel>
+        )
+    }
+}
+
+export default Image;

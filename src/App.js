cf686f83b004cec778033dcdd00ea736c3d13ba4
Bulava Eduard <bulavaeduard@gmail.com>
2017-10-23 09:16:44 +0300

    dropdown is back :)

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.request();
+        this.request(456);
         this.state = {
+            value: 456,
             teams: []
         }
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    request(){
-        const FETCH_URL = 'http://api.football-data.org/v1/competitions/464/teams';
+    request(id){
+        const FETCH_URL = 'http://api.football-data.org/v1/competitions/' + id +'/teams';
 
         fetch('https://cors-anywhere.herokuapp.com/'+ FETCH_URL, {
             method: 'GET',
@@ -27,6 +30,11 @@ class App extends Component {
                 const teams = json.teams;
                 this.setState({ teams });
             });
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+        this.request(this.state.value);
     }
 
     render() {
@@ -44,6 +52,16 @@ class App extends Component {
                         )
                     })}
                 </Carousel>
+                <form>
+                    <label>
+                        Pick a League:
+                        <select value={this.state.value} onChange={this.handleChange}>
+                            <option value="456">Serie A 2017/18</option>
+                            <option value="464">Champions League</option>
+                            <option value="457">Primeira Liga</option>
+                        </select>
+                    </label>
+                </form>
             </div>
         )
     }
